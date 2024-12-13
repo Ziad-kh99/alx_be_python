d591cb55246bcaf8001ae695afccef0d91a7a6f7
Ziad <ziadkh99@hotmail.com>
2024-12-13 13:40:59 +0200

    Fix Bug: recoder vaiables order in monthly_savings formula

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = int(input('Enter your monthly income: '))
 monthly_expenses = int(input('Enter your total monthly expenses: '))
 
-monthly_savings = monthly_expenses - monthly_income 
+monthly_savings = monthly_income - monthly_expenses 
 projected_annual_savings = int((monthly_savings * 12) + (monthly_savings * 12 * 0.05))
 
 print(f'Your monthly savings are ${monthly_savings}.')
