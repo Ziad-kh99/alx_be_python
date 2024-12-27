163714329ee5f4f04fd5e8160c45a9b5454ccb37
Ziad <ziadkh99@hotmail.com>
2024-12-27 15:08:52 +0200

    Midification: modfiy reminder message

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,19 +5,22 @@ time_bound = input('Is it time-bound? (yes/no): ')
 match priority:
     case 'high':
         if time_bound == 'yes':
-            print(f"'{task}' is a high priority task that requiers immediate attention today!")
+            print(f"Reminder: '{task}' is a {priority} priority task that requiers immediate attention today!")
         else:
-            print(f"'{task}' is a high priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+
     case 'medium':
         if time_bound == 'yes':
-            print(f"'{task}' is a {priority} priority task that requiers immediate attention today!")
+            print(f"Reminder: '{task}' is a {priority} priority task that requiers immediate attention today!")
         else:
-            print(f"'{task}' is a {priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+
     case 'low':
         if time_bound == 'yes':
-            print(f"'{task}' is a {priority} priority task that requiers immediate attention today!")
+            print(f"Reminder: '{task}' is a {priority} priority task that requiers immediate attention today!")
         else:
-            print(f"'{task}' is a {priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+
     case _:
         print('Invalid priority!')
 
