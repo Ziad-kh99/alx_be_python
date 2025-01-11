5ea3bf8669f95a47a0aa61f57b562949688320c5
Ziad Khaled <ziadkh99@hotmail.com>
2025-01-11 21:29:23 +0200

    Modification: Use if-else statemnts insted of match-case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            try:
-                return num1 / num2
-            except:
-                print('Invalid division by Zero operation.')
-                exit()
-        case _:
-            print('Invalid Operation.')
-
-            
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2 
+    elif operation == 'multiply':
+        return num1 * num2 
+    elif operation == 'divide':
+        try:
+            return num1 / num2
+        except:
+            print('Invalid division by Zero operation')
+    else:
+        print('Invalid operation')
