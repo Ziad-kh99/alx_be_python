621b8af3529682733f9a5935931cd7e11b391642
Ziad Khaled <ziadkh99@hotmail.com>
2025-01-11 21:33:21 +0200

    Modification: Use if-else insted of try-except to check divide by Zero scenario

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,13 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2 
     elif operation == 'divide':
-        try:
-            return num1 / num2
-        except:
+        # try:
+        #     return num1 / num2
+        # except:
+        #     print('Invalid division by Zero operation')
+        if num2 == 0:
             print('Invalid division by Zero operation')
+        else:
+            return num1 / num2
     else:
         print('Invalid operation')
