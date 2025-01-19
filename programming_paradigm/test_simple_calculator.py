6d25bff8fb3f6ce533f0a5b9701d2570422b2ae5
Ziad <ziadkh99@hotmail.com>
2025-01-19 15:17:22 +0200

    Modification: Modify unit tests code structure

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,21 +6,17 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        result = self.calc.add(5, 2)
-        self.assertEqual(result, 7)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(5, 2), 7)
 
-    def test_subtract(self):
-        result = self.calc.subtract(4, 7)
-        self.assertEqual(result, -3)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(4, 7), -3)
 
     def test_multiply(self):
-        result = self.calc.multiply(7, 7)
-        self.assertEqual(result, 49)
+        self.assertEqual(self.calc.multiply(7, 7), 49)
     
     def test_divide(self):
-        result = self.calc.divide(12, 3)
-        self.assertEqual(result, 4)
+        self.assertEqual(self.calc.divide(12, 3), 4)
         
         self.assertRaises(ZeroDivisionError, self.calc.divide, 4, 0)
     
